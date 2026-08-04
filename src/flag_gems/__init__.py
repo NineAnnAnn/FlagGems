a11bc260113af33259771ba18d9c2a0c0523c088
NineAnnAnn <dzw13054973993@163.com>
2026-08-04 16:40:11 +0800

    [KernelGen][Nvidia] Add addmv_ operator

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -198,6 +198,7 @@ _FULL_CONFIG = (
     ("addmm.out", addmm_out),
     ("addmm_", addmm_),
     ("addmv", addmv),
+    ("addmv_", addmv_),
     ("addmv.out", addmv_out),
     ("addr", addr),
     ("affine_grid_generator", affine_grid_generator),
