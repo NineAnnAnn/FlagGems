129f18846f3ce33a1cb07d082e91b92754dc792a
NineAnnAnn <dzw13054973993@163.com>
2026-08-05 13:36:10 +0800

    fix: remove upcast in test_negative_ to fix dtype mismatch

--- tests/test_negative_.py
+++ tests/test_negative_.py
@@ -26,7 +26,7 @@ from . import accuracy_utils as utils
 def test_negative_(shape, dtype):
     inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
 
-    ref_inp = utils.to_reference(inp.clone(), True)
+    ref_inp = utils.to_reference(inp.clone())
     ref_out = ref_inp.negative_()
 
     with flag_gems.use_gems():
