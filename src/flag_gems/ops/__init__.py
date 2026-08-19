708786f2f7b0b172423a370b8798a238bc7b8bf6
ZhiwenDeng <dzw13054973993@163.com>
2026-08-19 18:56:47 +0800

    [KernelGen][Nvidia] Add choose_qparams_optimized operator with Triton kernel
    Signed-off-by: ZhiwenDeng <dzw13054973993@163.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -255,6 +255,7 @@ from flag_gems.ops.celu import celu, celu_
 from flag_gems.ops.channel_shuffle import channel_shuffle
 from flag_gems.ops.cholesky_inverse import cholesky_inverse
 from flag_gems.ops.cholesky_solve import cholesky_solve, cholesky_solve_out
+from flag_gems.ops.choose_qparams_optimized import choose_qparams_optimized
 from flag_gems.ops.chunk import chunk
 from flag_gems.ops.clamp import (
     clamp,
@@ -1109,6 +1110,7 @@ __all__ = [
     "cholesky_inverse",
     "cholesky_solve",
     "cholesky_solve_out",
+    "choose_qparams_optimized",
     "chunk",
     "clamp",
     "clamp_",
