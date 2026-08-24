270ec7bebc58d98783cdff1da7f46db14256020e
Dongxu-H <dxhan@baai.ac.cn>
2026-08-24 08:05:44 +0000

    [KernelGen][Nvidia] Add matrix_exp_backward operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -771,6 +771,7 @@ _FULL_CONFIG = (
     ("masked_scatter_backward", masked_scatter_backward),
     ("masked_select", masked_select),
     ("matmuladd", matmuladd),
+    ("matrix_exp_backward", matrix_exp_backward),
     ("max", max),
     ("max.dim", max_dim),
     ("max_pool2d_backward", max_pool2d_backward),
