69da1bc71162cbb20d525bdcebf8edd4f5a044bc
ZhiwenDeng <dzw13054973993@163.com>
2026-08-27 05:07:49 +0000

    [KernelGen][Nvidia] Add trace_backward operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -867,6 +867,7 @@ from flag_gems.ops.tile import tile
 from flag_gems.ops.to import to_copy
 from flag_gems.ops.topk import topk
 from flag_gems.ops.trace import trace
+from flag_gems.ops.trace_backward import trace_backward
 from flag_gems.ops.transpose import transpose
 from flag_gems.ops.tril import tril, tril_, tril_out
 from flag_gems.ops.triu import triu, triu_
@@ -1503,6 +1504,7 @@ __all__ = [
     "logaddexp2_out",
     "logcumsumexp",
     "logcumsumexp_out",
+    "trace_backward",
     "unflatten",
     "unsafe_chunk",
     "xlogy",
